a553aa22dc0862254a0dbb3fb9ed5f6e9a7b92ff
Poulami <poulami.chaudhury123@gmail.com>
2024-11-17 03:10:31 +0530

    dependency fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 firebase.initializeApp({
-    
+    apiKey: "AIzaSyCRau9FEIsm1sBUjgQTHQzsD8pCWf0DKpk",
+    authDomain: "myblogsite-ed3b2.firebaseapp.com",
+    projectId: "myblogsite-ed3b2",
+    storageBucket: "myblogsite-ed3b2.firebasestorage.app",
+    messagingSenderId: "325306585921",
+    appId: "1:325306585921:web:1762f841e8a4c436b647c0",
+    measurementId: "G-MSVJ8HNRF8"
 });
 
 const fb = firebase;
